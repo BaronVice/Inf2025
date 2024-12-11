3230365dc94a3a2eb854d48704d5474012380c31
Tony <toni.sas.228@mail.ru>
2024-12-11 20:10:30 +0800

    I want to tell you all there is to know about the end of time

---
+++ 16/demo.py
@@ -0,0 +1,14 @@
+from sys import setrecursionlimit
+setrecursionlimit(10**8)
+
+def F(n):
+    # Теперь распишем, что при значении n функция будет возвращать
+    # F(n) = 1 при n = 1:
+    if n == 1:
+        return 1
+    # F(n) = (n – 1) x F(n - 1), если n > 1
+    if n > 1:
+        return (n - 1) * F(n - 1)
+
+# Чему равно значение выражения (F(2024) + 2 x F(2023)) / F(2022)
+print( (F(2024) + 2 * F(2023)) / F(2022) )
